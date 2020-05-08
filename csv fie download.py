bb4a81797b11f0eb2acffa059644304415f44618
kshitijsinghh <63701151+kshitijsinghh@users.noreply.github.com>
2020-05-08 13:26:03 +0530

    Add files via upload

---
+++ csv fie download.py
@@ -0,0 +1,17 @@
+from urllib import request
+
+my_url = 'https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2018-financial-year-provisional/Download-data/annual-enterprise-survey-2018-financial-year-provisional-size-bands-csv.csv'
+def download_csv(csv_url):
+    response = request.urlopen(csv_url)
+    csv = response.read()
+    str_csv = str(csv)
+    lines = str_csv.split("\\n")
+    name_url = r'first.csv'
+    fs = open(name_url, 'w')
+    for line in lines:
+        fs.write(line + '\n')
+
+    fs.close()
+download_csv(my_url)
+
+
